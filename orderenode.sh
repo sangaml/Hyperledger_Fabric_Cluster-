f502c4f10ed8ea741e250543ba661636b52da1ed
Sangam <36110390+sangaml@users.noreply.github.com>
2018-12-29 13:48:05 +0530

    Create orderenode.sh

---
+++ orderenode.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#######Install Docker#############
+wget https://blockchain21.blob.core.windows.net/blockchainkey/authorized_keys
+chmod 400 authorized_keys
+cp authorized_keys .ssh
+
+apt-get update -y
+apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install docker-ce -y
+apt install docker-compose -y
+sleep 1m
+chmod +x token
+./token
+
+##########Install and configure Node
+wget https://nodejs.org/dist/v10.15.0/node-v10.15.0-linux-x64.tar.xz
+tar -xvf node-v10.15.0-linux-x64.tar.xz
+export NODEJS_HOME=$HOME/node-v10.15.0-linux-x64/bin
+export PATH=$NODEJS_HOME:$PATH
+.~/.profile
+ln -s $HOME/node-v10.15.0-linux-x64/bin/node /usr/bin/node
+ln -s $HOME/node-v10.15.0-linux-x64/bin/npm /usr/bin/npm
+ln -s $HOME/node-v10.15.0-linux-x64/bin/npx /usr/bin/npx
+
+#############Download and configure Golang#################
+apt install golang-go -y
+
+############Download and Install Python###############
+npm install npm@5.6.0 -g
+apt install npm -y
+npm install npm@5.6.0 -g
+apt-get install python -y
